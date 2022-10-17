0eb649deab23e064811565a6e8806a5958f00e89
Pitta Junior <pitekodejesus@gmail.com>
2022-10-17 11:38:05 -0300

    Implemento do tabuleiro e peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,16 @@
 package application;
 
+import boardGame.Board;
 import boardGame.Position;
 
 public class Program {
 	public static void main(String[] args) {
 		
 		
-		Position pos = new Position(2,5);
+		Board board = new Board(8,8);
 		
-		System.out.println(pos);
+		
+		
 	}
 
 }
